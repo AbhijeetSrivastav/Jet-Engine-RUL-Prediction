0a6e6a50eaae60991be58092c203c39109d87f84
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2023-03-30 16:19:58 +0530

    Data Ingestion Artifact added

---
+++ rul/entity/artifact_entity.py
@@ -0,0 +1,19 @@
+"Contain all the artifacts of the components and pipelines"
+
+
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionArtifact:
+    """
+    Artifact for Data Ingestion component
+    ----------------------------------------------------
+    return:
+    `feature_store_file_path`: path of feature store of data ingestion component
+    `train_file_path`: path of train file created by Data Ingestion component
+    `test_file_path`: path of test file created by Data Ingestion component
+    """
+    feature_store_file_path: str
+    train_file_path: str
+    test_file_path: str
