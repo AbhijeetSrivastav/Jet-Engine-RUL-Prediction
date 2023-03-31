6afddeea2a1b7abf044a83e64f738e7bfbd693f9
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2023-03-31 11:56:28 +0530

    Data Ingestion Configuration added

--- rul/entity/config_entity.py
+++ rul/entity/config_entity.py
@@ -31,3 +31,36 @@ class TrainingPipelineConfig:
             raise RULException(e, sys)
         
 
+class DataIngestionConfig:
+    """
+    Configuration for Data Ingestion Component
+    -----------------------------------------------------------------
+    input:
+    - `training_pipeline_config`: configuration of training pipeline
+    -----------------------------------------------------------------
+    return: dictionary of everything defined in constructor
+    """
+
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig) -> dict:
+        try:
+            self.database_name = "rul"
+            self.collection_name = "rul_collect"
+
+            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir, "data_ingestion")
+
+            self.feature_store_path = os.path.join(self.data_ingestion_dir, "feature_store", FILE_NAME)
+
+            self.train_file_path = os.path.join(self.data_ingestion_dir, "dataset", TRAIN_FILE_NAME)
+
+            self.test_file_path = os.path.join(self.data_ingestion_dir, "dataset", TEST_FILE_NAME)
+
+            self.test_size = 0.2
+
+        except Exception as e:
+            raise RULException(e, sys)
+        
+        def to_dict(self)->dict:
+            try:
+                return self.___dict__
+            except Exception as e:
+                raise RULException(e, sys)
