7c4f6dfc43ba9de320b80b0bae7289b26c87e4af
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2023-04-03 13:27:20 +0530

    Data Validation Component added to training pipeline

--- rul/pipeline/training_pipeline.py
+++ rul/pipeline/training_pipeline.py
@@ -7,6 +7,7 @@ from rul.logger import logging
 from rul.exception import RULException
 from rul.entity import config_entity
 from rul.components.data_ingestion import DataIngestion
+from rul.components.data_validation import DataValidation
 
 
 
@@ -23,6 +24,14 @@ def start_training_pipeline():
         data_ingestion = DataIngestion(data_ingestion_config = data_ingestion_config)
 
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+        # Data Validation 
+        logging.info(f"-----------------Initiating Data Validation-----------------")
+        data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+
+        data_validation = DataValidation(data_validation_config=data_validation_config, data_ingestion_artifact=data_ingestion_artifact)
+
+        data_validation_artifact = data_validation.initiate_data_validation()
 
     except Exception as e:
         raise RULException(e, sys)
