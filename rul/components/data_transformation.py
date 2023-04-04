b0d304bcc228ccf715ce9a387d15eaca15238091
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2023-04-04 18:02:37 +0530

    Data Transformation Initiatae method added to Data Transformation Component

--- rul/components/data_transformation.py
+++ rul/components/data_transformation.py
@@ -10,7 +10,6 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.impute import SimpleImputer
 from rul.entity import config_entity
 from rul.entity import artifact_entity
-from rul.config import TARGET_COLUMN
 from rul.exception import RULException
 from rul.logger import logging
 from rul import utils
@@ -99,12 +98,72 @@ class DataTransformation:
         return: Data Transformation Artifact
         """
 
-        # Reading train file
-        logging.info(f"Reading train file")
-        train_df = pd.read_csv(self.data_ingestion_artifact.train_file_path)
+        try:
+            # Reading train file
+            logging.info(f"Reading train file")
+            train_df = pd.read_csv(self.data_ingestion_artifact.train_file_path)
 
-        # Reading test file 
-        logging.info(f"Reading test file")
-        test_df = pd.read_csv(self.data_ingestion_artifact.test_file_path)
+            # Reading test file 
+            logging.info(f"Reading test file")
+            test_df = pd.read_csv(self.data_ingestion_artifact.test_file_path)
 
-        # Adding RUL feature to train and test dataFrame
+            # Adding RUL feature to train dataFrame
+            logging.info(f"Adding RUL to train dataFrame")
+            train_df = self.add_RUL_feature(train_df)
+            
+            # Adding RUL feature to test dataFrame
+            logging.info(f"Adding RUL to test dataFrame")
+            test_df = self.add_RUL_feature(test_df)
+
+            # Defining irrelevant features aka: index, setting and constant sensor 
+            logging.info(f"Proceeding to drop irrelevant index, setting and constant sensor features")
+            index_names = ["unit_number", "time_cycles"]
+            setting_names = ["setting_1", "setting_2", "setting_3"]
+            constant_sensors = ["s_1", "s_5",'s_6', "s_10", "s_16", "s_18", "s_19"]
+
+            drop_labels = index_names + setting_names + constant_sensors
+
+            # Dropping irrelevant features from train dataFrame
+            logging.info(f"Dropping irrelevant features from train dataFrame")
+            train_df.drop(columns=drop_labels, inplace=True)
+            
+            # Dropping irrelevant features from test dataFrame
+            logging.info(f"Dropping irrelevant features from test dataFrame")
+            test_df.drop(columns=drop_labels, inplace=True) 
+
+            # Fetching and Fitting transformation pipeline
+            logging.info(f"Fetching transformation object, creating and fitting transformation pipeline")
+            transformation_pipeline = DataTransformation.get_data_transformer_object()
+            transformation_pipeline.fit(train_df)
+
+            # Transforming train dataFrame
+            logging.info(f"Transforming train dataFrame")
+            train_arr = transformation_pipeline.transform(train_df)
+
+            # Transforming train dataFrame
+            logging.info(f"Transforming test dataFrame")
+            test_arr = transformation_pipeline.transform(test_df)
+
+            # Saving train array as artifact
+            logging.info(f"Saving train array as artifact")
+            utils.save_numpy_array_data(file_path=self.data_transformation_config.data_transformed_train_path, array=train_arr)
+
+            # Saving test array as artifact
+            logging.info(f"Saving test array as artifact")
+            utils.save_numpy_array_data(file_path=self.data_transformation_config.data_transformed_test_path, array=test_arr)
+
+            # Saving transformer object
+            utils.save_object(file_path=self.data_transformation_config.data_transformer_object_path, obj=transformation_pipeline)
+
+            # Preparing artifact
+            logging.info(f"Preparing Data Transformation Artifact")
+            data_transformation_artifact = artifact_entity.DataTransformationArtifact(
+                                                                        transformer_object_path=self.data_transformation_config.data_transformer_object_path,
+                                                                        transformed_train_path=self.data_transformation_config.data_transformed_train_path,
+                                                                        transformed_test_path=self.data_transformation_config.data_transformed_test_path
+                                                                        )
+            
+            return data_transformation_artifact
+
+        except Exception as e:
+            raise RULException(e, sys)
