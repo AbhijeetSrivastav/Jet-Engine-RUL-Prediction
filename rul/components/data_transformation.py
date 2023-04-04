6ad29e0458f53df4a14ab04f54167b3d3fe49b42
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2023-04-04 12:39:45 +0530

    Add RUL method added to Data Transformation Component

--- rul/components/data_transformation.py
+++ rul/components/data_transformation.py
@@ -62,4 +62,49 @@ class DataTransformation:
         
         except Exception as e:
             raise RULException(e, sys)
+    
+    def add_RUL_feature(df: pd.DataFrame) -> pd.DataFrame:
+        """"
+        Adds target feature RUL to the dataframe
+        ---------------------------------------------
+        input:
+        -  `df`: dataframe in which to add RUL
+        ----------------------------------------------
+        return: `pd.Dataframe`
+        """
 
+        try:
+            train_grouped_by_unit = df.groupby(by='unit_number') 
+
+            max_time_cycles = train_grouped_by_unit['time_cycles'].max() 
+
+            merged = df.merge(max_time_cycles.to_frame(name='max_time_cycle'), left_on='unit_number',right_index=True)
+
+            merged["RUL"] = merged["max_time_cycle"] - merged['time_cycles']
+
+            merged = merged.drop("max_time_cycle", axis=1) 
+
+            return merged
+        
+        except Exception as e:
+            raise RULException(e, sys)
+
+    def initiate_data_transformation(self) -> artifact_entity.DataTransformationArtifact:
+        """
+        Initiates Data Transformation Component
+        ----------------------------------------------
+        input:
+        - `None`
+        -----------------------------------------------
+        return: Data Transformation Artifact
+        """
+
+        # Reading train file
+        logging.info(f"Reading train file")
+        train_df = pd.read_csv(self.data_ingestion_artifact.train_file_path)
+
+        # Reading test file 
+        logging.info(f"Reading test file")
+        test_df = pd.read_csv(self.data_ingestion_artifact.test_file_path)
+
+        # Adding RUL feature to train and test dataFrame
