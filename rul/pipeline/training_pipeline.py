2038cdd7d6b058dfe90b43f91749053e1fbb8ca4
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2023-04-04 18:06:08 +0530

    Data Transformation Component added to Training Pipeline

--- rul/pipeline/training_pipeline.py
+++ rul/pipeline/training_pipeline.py
@@ -8,6 +8,7 @@ from rul.exception import RULException
 from rul.entity import config_entity
 from rul.components.data_ingestion import DataIngestion
 from rul.components.data_validation import DataValidation
+from rul.components.data_transformation import DataTransformation
 
 
 
@@ -32,6 +33,13 @@ def start_training_pipeline():
         data_validation = DataValidation(data_validation_config=data_validation_config, data_ingestion_artifact=data_ingestion_artifact)
 
         data_validation_artifact = data_validation.initiate_data_validation()
+
+        # Data Transformation
+        data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+
+        data_transformation = DataTransformation(data_transformation_config=data_transformation_config, data_ingestion_artifact=data_ingestion_artifact)
+
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
 
     except Exception as e:
         raise RULException(e, sys)
