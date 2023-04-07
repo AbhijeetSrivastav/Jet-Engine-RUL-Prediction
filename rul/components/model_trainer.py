ba625c0fbb8bf1e06141b8e099bbb072b0ae2b15
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2023-04-07 17:25:51 +0530

    Train Model added to Model Trainer Component

--- rul/components/model_trainer.py
+++ rul/components/model_trainer.py
@@ -30,6 +30,32 @@ class ModelTrainer:
             self.model_trainer_config = model_trainer_config
 
             self.data_transformation_artifact = data_transformation_artifact
+
+        except Exception as e:
+            raise RULException(e, sys)
+    
+    def train_model(self, x: np.array, y: np.array) -> RandomForestRegressor:
+        """
+        Creates a Random Forest Regressor which was selected as the best model during experimentation
+        -------------------------------------------------------------------------------------------------
+        input:
+        - `x`: input feature array
+        - `y`: target feature array
+        ---------------------------------------------------------------------------------------------------
+        return: `RandomForestRegressor`
+        """
+
+        try:
+            logging.info(f"Creating instance of RandomForestRegressor")
+            rfr = RandomForestRegressor(
+                                        criterion="poisson", max_features="sqrt",
+                                        ccp_alpha=0.0, n_estimators=100
+                                        )
             
+            logging.info(f"Fitting the RandomForestRegressor model")
+            rfr.fit(x, y)
+
+            return rfr
+        
         except Exception as e:
             raise RULException(e, sys)
